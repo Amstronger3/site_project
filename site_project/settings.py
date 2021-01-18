27d6a8cd327775543a2a818c5612d94976ea5186
Amstronger3 <amstronger3@gmail.com>
2021-01-18 19:56:23 +0200

    first commit for all files

--- site_project/settings.py
+++ site_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'cinema'
+    'cinema',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
