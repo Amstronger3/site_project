bac08f35c265a52267b62072471e6e7079170ca4
Amstronger3 <amstronger3@gmail.com>
2021-01-18 20:14:45 +0200

    first commit for all files

--- site_project/settings.py
+++ site_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_user_agents',
-    'tracking_analyzer',
+    # 'django_user_agents',
+    # 'tracking_analyzer',
 ]
 
 MIDDLEWARE = [
